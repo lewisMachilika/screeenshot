1b75c3d2d94a87d280ef40567547fcf645d50835
machilika.l <lewis.machilika@nbs.mw>
2020-12-11 14:27:35 +0200

    luja

--- fabfile.py
+++ fabfile.py
@@ -1,12 +1,25 @@
 #from fabric.api import env, run, local
 from fabric.api import local
+import os, sys
 
 #machine=input("machine: ")
 
 #env.hosts = [ '192.168.1.100', '192.168.1.101', '192.168.1.102' ]
 
 #env.hosts = [ machine ]
+'''
+from invoke import Responder
 
+git_watchers = [
+                Responder(pattern = r"Username for .*", response="lewis.machilika@nbs.mw\n"), 
+                Responder(pattern = r"Password for .*", response="#include<iostream>1993\n")
+               ]
+
+local("git fetch", watchers=git_watchers)
+'''
+import datetime
+now = datetime.datetime.now()
+os.rename("myfolder","myfolder{0}".format(now.strftime("%Y-%m-%d%H%M%S")))
 def test():
     local("./manage.py test my_app")
 
